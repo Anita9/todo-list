188d5d35e2a50b731420410003584b398cf0f6c9
Anita <raskovanita1@gmail.com>
2022-02-14 11:34:12 +0100

    Updates

---
+++ precache-manifest.8e135fd534af35da85a1c39770427f09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ef83b16db4d9f23d7ce3e885aa9829",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f78cd7e929ff245324d3",
+    "url": "/todo-list/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "19eaf865b95ac29a0d2a",
+    "url": "/todo-list/static/css/main.6c2b44fd.chunk.css"
+  },
+  {
+    "revision": "f78cd7e929ff245324d3",
+    "url": "/todo-list/static/js/2.dc6ba932.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.dc6ba932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19eaf865b95ac29a0d2a",
+    "url": "/todo-list/static/js/main.b907f03a.chunk.js"
+  },
+  {
+    "revision": "b8f023c4114e4cc4c614",
+    "url": "/todo-list/static/js/runtime-main.2918b0a5.js"
+  },
+  {
+    "revision": "5cf160152619b4101b8480bf98102dd4",
+    "url": "/todo-list/static/media/todo.5cf16015.png"
+  }
+]);
